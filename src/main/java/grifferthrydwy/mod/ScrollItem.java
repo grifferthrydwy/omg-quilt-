c475d871da32fdad79c0eea86aafec0f5a8010a4
grifferthrydwy <78763622+grifferthrydwy@users.noreply.github.com>
2022-04-24 13:54:47 -0400

    hi cmd

--- src/main/java/grifferthrydwy/mod/ScrollItem.java
+++ src/main/java/grifferthrydwy/mod/ScrollItem.java
@@ -1,7 +1,20 @@
 package grifferthrydwy.mod;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
 
 public class ScrollItem extends Item {
@@ -18,7 +31,7 @@ public class ScrollItem extends Item {
         SPELL_TWO,
         SPELL_THREE
     }
-    public int castSpell(ItemStack stack) {
+    public void castSpell(PlayerEntity user) {
         Spell spell = this.spell;
 
         switch (spell) {
@@ -26,10 +39,41 @@ public class ScrollItem extends Item {
             case SPELL_TWO -> spellTwo.run();
             case SPELL_THREE -> spellThree.run();
         }
-        return 0;
+    }
+    private boolean tick = false;
+    private int tickCounter = 0;
+    private ServerPlayerEntity player;
+    private ClientPlayerEntity clientPlayer;
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
+        if (playerEntity instanceof ServerPlayerEntity) player = (ServerPlayerEntity) playerEntity; // Get server player
+        else if (playerEntity instanceof ClientPlayerEntity) clientPlayer = (ClientPlayerEntity) playerEntity; // Get client player
+
+        if (!tick) {
+            final MinecraftClient client = MinecraftClient.getInstance();
+            // client.particleManager.addEmitter(clientPlayer, ParticleTypes.END_ROD, 30);
+            client.gameRenderer.showFloatingItem(this.getDefaultStack());
+            if (player != null)
+                player.getWorld().playSound(playerEntity, playerEntity.getBlockPos(), SoundEvents.ENCHANT_THORNS_HIT, SoundCategory.PLAYERS, 1f, 1f);
+            tick = true;
+            return TypedActionResult.success(playerEntity.getStackInHand(hand));
+        } else {
+            return TypedActionResult.fail(playerEntity.getStackInHand(hand));
+        }
     }
 
-
+    @Override
+    public void inventoryTick(ItemStack stack, World world, Entity entity, int slot, boolean selected) {
+        if (player != null && tick) {
+            tickCounter++;
+            if (tickCounter >= 30) {
+                castSpell(player);
+                tick = false;
+                tickCounter = 0;
+            }
+        }
+    }
 
 
 
